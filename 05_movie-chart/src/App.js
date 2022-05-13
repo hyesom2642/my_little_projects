795b27764a3a83d5c59b6fb3cae6c8c35ee9def4
hyesom2642 <kimhyesom@naver.com>
2022-05-13 16:46:39 +0900

    🚚 Replace router for new version(v6)

--- 05_movie-chart/src/App.js
+++ 05_movie-chart/src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router , Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router , Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from 'recoil';
 import Home from './routes/Home.js';
 import Detail from './routes/Detail.js';
 
@@ -6,16 +8,26 @@ import './App.css';
 
 function App(){
   return (
-    <Router>
-      <Switch>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Nav />
+    //   <Switch>
+    //     <Route path="/movie/:id">
+    //       <Detail />
+    //     </Route>
+    //     <Route path="/">
+    //       <Home />
+    //     </Route>
+    //   </Switch>
+    // </Router>
+    <RecoilRoot>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/movie/:id" element={<Detail />}/>
+        </Routes>
+      </Router>
+    </RecoilRoot>
+
   )
 }
 export default App;
