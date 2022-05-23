5139c0e1a689735f5010776dcc5e8261db941475
hyesom2642 <kimhyesom@naver.com>
2022-05-23 21:26:22 +0900

    ✨ Implement & style input part

--- 04_todolist/src/Components/Input.js
+++ 04_todolist/src/Components/Input.js
@@ -1,83 +1,94 @@
 // > Css
-import styled from 'styled-components';
+import InputStyle from '../Css/Input.module.css';
+// import styled from 'styled-components';
 
-// > 
-import { useState } from 'react';
+// >
+import { useState, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-function Input(){
-    const [toDo, setTodo] = useState("");
-    const [toDos, setTodos] = useState([]);
+function Input({ todoList, setTodoList }) {
+  const [toDo, setTodo] = useState('');
+  const inputRef = useRef(null); // # useRef() : Hook으로 ref 생성
 
-    // > 
-    const onChange = (e) => setTodo(e.target.value);
-    const submitFn = (e) => {
-        e.preventDefault();
-        if( toDo === "" ){
-            return;
-        }
-        setTodo("");
-        setTodos((currentItems) => [...currentItems, toDo]);
-    }
-    const deleteItemFn = (e) => {
-        const li = e.target.parentElement;
-        li.remove();
-    }
+  // # input 값 가져오기
+  const onChange = (e) => {
+    setTodo(e.target.value);
+  };
 
-    // > styled components 
-    const OlContainer = styled.ol`
-        width: 300px;
-        list-style: none;
-        padding-left: 0;
-    `;
-    const DeleteBtn = styled.button`
-        background: inherit;
-        border: none;
-        cursor: pointer;
-    `;
-    const formStyle = {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-    }
-    const inputStyle = {
-        width: "300px",
-        height: "10px",
-        padding: "10px",
-        outline: "none",
-        border: "none",
-        borderBottom: "1px solid #eee",
-    }
-    const buttonStyle = {
-        padding: "10px",
-        backgroundColor: "inherit",
-        outline: "none",
-        border: "none",
-        fontSize: "25px",
-        fontWeight: "bold",
-        cursor: "pointer",
-    }
+  // # addBtn을 클릭했을 때
+  const addBtnClick = (e) => {
+    e.preventDefault();
+    // # todoitem 추가
+    // # input에 입력한 값을 todoList에 추가, concat(): 인자로 받은 값을 배열에 추가하여 새로운 배열을 반환한다.
+    // # id : item마다 id를 넣어준다.
+    // # toDo : 각 todo item의 내용
+    const nextTodoList = todoList.concat({
+      id: todoList.length,
+      toDo,
+      checked: false,
+    });
+    setTodoList(nextTodoList);
 
-    return(
-        <>
-            <div>
-                <form onSubmit={ submitFn } type="submit" style={formStyle} >
-                    <input type="text" value={toDo} onChange={onChange} placeholder="오늘의 할 일을 적어주세요" style={inputStyle} />
-                    <button style={buttonStyle}>+</button>
-                </form>
-                <OlContainer>
-                {
-                    toDos.map((item, index) => (
-                        <li key={index}>
-                            <input type="checkbox" />
-                            {item}
-                            <DeleteBtn onClick={deleteItemFn}>❌</DeleteBtn>
-                        </li>
-                    ))
-                }
-                </OlContainer>
-            </div>
-        </>
-    )
+    // # input값을 초기화 & focus
+    setTodo('');
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
+
+  //   const submitFn = (e) => {
+  //     e.preventDefault();
+  //     if (toDo === '') {
+  //       return;
+  //     }
+  //     setTodo('');
+  //     setTodos((currentItems) => [...currentItems, toDo]);
+  //   };
+  //   const deleteItemFn = (e) => {
+  //     const li = e.target.parentElement;
+  //     li.remove();
+  //   };
+
+  return (
+    <>
+      <form classNmae={InputStyle.formStyle} type="submit">
+        <input
+          className={InputStyle.input}
+          type="text"
+          value={toDo}
+          onChange={onChange}
+          ref={inputRef}
+          placeholder="오늘의 할 일을 적어주세요"
+        />
+        <button className={InputStyle.addBtn} onClick={addBtnClick}>
+          +
+        </button>
+      </form>
+
+      {/* <OlContainer>
+          {toDos.map((item, index) => (
+            <li key={index}>
+              <input type="checkbox" />
+              {item}
+              <DeleteBtn onClick={deleteItemFn}>❌</DeleteBtn>
+            </li>
+          ))}
+        </OlContainer> */}
+    </>
+  );
 }
+
+// > proptypes
+Input.propTypes = {
+  todoList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      todo: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+  setTodoList: PropTypes.func.isRequired,
+};
 
 export default Input;
