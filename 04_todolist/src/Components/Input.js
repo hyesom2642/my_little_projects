483bffdfa39a48e19cd18fe6e38e6332e3c1e065
hyesom2642 <kimhyesom@naver.com>
2022-05-23 11:36:41 +0900

    💄 Styled add button & input box

--- 04_todolist/src/Components/Input.js
+++ 04_todolist/src/Components/Input.js
@@ -7,8 +7,9 @@ import { useState } from 'react';
 function Input(){
     const [toDo, setTodo] = useState("");
     const [toDos, setTodos] = useState([]);
-    const onChange = (e) => setTodo(e.target.value);
 
+    // > 
+    const onChange = (e) => setTodo(e.target.value);
     const submitFn = (e) => {
         e.preventDefault();
         if( toDo === "" ){
@@ -17,36 +18,57 @@ function Input(){
         setTodo("");
         setTodos((currentItems) => [...currentItems, toDo]);
     }
-
     const deleteItemFn = (e) => {
         const li = e.target.parentElement;
         li.remove();
     }
 
+    // > styled components 
     const OlContainer = styled.ol`
-    width: 300px;
+        width: 300px;
+        list-style: none;
+        padding-left: 0;
     `;
     const DeleteBtn = styled.button`
         background: inherit;
+        border: none;
         cursor: pointer;
     `;
+    const formStyle = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    }
     const inputStyle = {
         width: "300px",
         height: "10px",
-
+        padding: "10px",
+        outline: "none",
+        border: "none",
+        borderBottom: "1px solid #eee",
+    }
+    const buttonStyle = {
+        padding: "10px",
+        backgroundColor: "inherit",
+        outline: "none",
+        border: "none",
+        fontSize: "25px",
+        fontWeight: "bold",
+        cursor: "pointer",
     }
 
     return(
         <>
             <div>
-                <form onSubmit={ submitFn } type="submit">
+                <form onSubmit={ submitFn } type="submit" style={formStyle} >
                     <input type="text" value={toDo} onChange={onChange} placeholder="오늘의 할 일을 적어주세요" style={inputStyle} />
-                    <button >+</button>
+                    <button style={buttonStyle}>+</button>
                 </form>
                 <OlContainer>
                 {
                     toDos.map((item, index) => (
                         <li key={index}>
+                            <input type="checkbox" />
                             {item}
                             <DeleteBtn onClick={deleteItemFn}>❌</DeleteBtn>
                         </li>
