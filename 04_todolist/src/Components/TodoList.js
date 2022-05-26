3539c25b020dffe607518959186b4ea189111232
hyesom2642 <kimhyesom@naver.com>
2022-05-26 20:07:30 +0900

    ✨ Implement TodoList component & style

--- 04_todolist/src/Components/TodoList.js
+++ 04_todolist/src/Components/TodoList.js
@@ -2,32 +2,27 @@
 import TodoItem from './TodoItem';
 
 // > Css
-import listStyle from '../Css/list.module.css';
+import listStyle from '../Css/TodoList.module.css';
 
-function TodoList({ title, todoList, setTodoList, checked }) {
+function TodoList({ todoList, setTodoList }) {
   return (
     <>
       <div className={listStyle.container}>
-        <h1 className={listStyle.title}>{title}</h1>
         <ul className={listStyle.list}>
-          {todoList &&
-            todoList.map((todoItem) => {
-              if (checked !== todoItem.checked) return null;
-              return (
-                <TodoItem
-                  key={todoItem.id}
-                  todoItem={todoItem}
-                  todoList={todoList}
-                  setTodoList={setTodoList}
-                />
-              );
-            })}
+          {todoList.map((todoItem) => {
+            return (
+              <TodoItem
+                key={todoItem.id}
+                todoItem={todoItem}
+                todoList={todoList}
+                setTodoList={setTodoList}
+              />
+            );
+          })}
         </ul>
       </div>
     </>
   );
 }
-
-// + 반복은 map()으로
 
 export default TodoList;
